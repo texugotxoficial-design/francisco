7b7cf9e16bcc581b2d92d84c295faa8c7ca66de8
Texugo <texugo@example.com>
2026-03-20 00:27:08 -0300

    fix: change default port to 80 for square cloud and increase memory

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 80;
 
 app.use(express.static(join(__dirname, 'dist')));
 
